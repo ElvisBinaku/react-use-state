38656d3a44185b6241ad538ff30f6006f659861c
ElvisBinaku <elvisbinaku@icloud.com>
2026-04-14 22:07:40 +0200

    Creata una serie di card che mostrano al loro interno un bottone.  Se il bottone viene cliccato, cambia colore e la descrizione diventa visibile all'interno della card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Card from './components/Card.jsx'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Card />
     </>
   )
 }
